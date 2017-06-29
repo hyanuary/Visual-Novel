4f68e9b22e02d7fcfea0f20f5c9f36309c966eb8
SAE\hyanuary <1003495@student.sae.edu.au>
2017-06-29 18:38:32 +1000

    visual novel init upload

---
+++ Assets/Scripts/CameraEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffect : MonoBehaviour {
+    public Material mat;
+    public float Fade = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mat.SetFloat("_Fade", Fade);
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}
---
+++ Assets/Scripts/TextEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextEffect : MonoBehaviour
+{
+    DialogueParser parser;
+    public float letterSpeed = 0.2f;
+    public int lineNum;
+
+    string dialogue;
+    public Text dialogueBox;
+
+    // Use this for initialization
+    void Start()
+    {
+        dialogue = dialogueBox.text;
+        StartCoroutine(TypeText());
+
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in dialogue.ToCharArray())
+        {
+            dialogueBox.text += letter;
+
+            yield return new WaitForSeconds(letterSpeed);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
